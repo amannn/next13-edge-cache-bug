9540bb4db77a126f306bb33ab577551703fd4218
Jan Amann <jan@amann.me>
2023-02-20 14:53:24 +0100

    Introduce cache-control

--- next.config.js
+++ next.config.js
@@ -3,5 +3,20 @@ module.exports = {
   reactStrictMode: true,
   experimental: {
     appDir: true
+  },
+  headers() {
+    return [
+      {
+        source: '/',
+        headers: [
+          {
+            // Cache for 1 second, but allow to send
+            // cached responses for up to 1 week
+            key: 'Cache-Control',
+            value: 's-maxage=1, stale-while-revalidate=604800'
+          }
+        ]
+      }
+    ];
   }
 };
